3a3fe7bf9abb4fc418cbd6bea1315242ca47b344
Corentin THOMASSET <corentin.thomasset74@gmail.com>
2017-05-04 22:31:40 +0200

    Better comments

--- main.py
+++ main.py
@@ -2,26 +2,33 @@
 # Home-made by CorentinTh
 # https://github.com/CorentinTh/ML-MyKNearestNeighbors
 
+# To get execution time
 import time
 start_time = time.time()
 
 from sklearn import datasets
 from MyKNN import *
 
+# We get the Iris dataset from the sklearn library
 iris = datasets.load_iris()
 
+# Features in x
+# and labels in y
 x = iris.data
 y = iris.target
 
+# We split our Iris dataset in two, one half to train our classifier, 
+# and the other half to test its accuracy
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .5)
 
-#from sklearn.neighbors import KNeighborsClassifier
 my_classifier = MyKNN()
 my_classifier.k = 5
+
+# We train the classifier with the first half of our data
 my_classifier.train(x_train, y_train)
-
+# We test it with features of the other half
 predictions = my_classifier.predict(x_test)
-
+# We compute the accuracy by comparing the output results with reals ones
 duration = time.time()-start_time
 print("Precision : {}% en {} sec".format(getAccuracy(y_test, predictions), duration))
