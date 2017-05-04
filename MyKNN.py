0f8f05d61e8d4afd5969fd2fbd7b0bd14645e124
Corentin <corentin.thomasset74@gmail.com>
2017-05-04 21:30:48 +0200

    K_implementation

---
+++ MyKNN.py
@@ -0,0 +1,79 @@
+import math
+import sys
+import operator
+
+# Compute a multi dimentional euclidean distance 
+def euclideanDistance(data1, data2):
+	if len(data1) != len(data2):
+		sys.exit("Error euclideanDistance : input doesn't have the same lenght")
+
+	dist = 0
+	for i in range(len(data1)):
+		dist += pow(data1[i] - data2[i], 2)
+	return math.sqrt(dist)
+
+class MyKNN():
+	# The constructor method
+	def __init__(self):
+		# Initialization of the k variable it representes
+		# the amount of neighbors we will check
+		self.k = 5
+
+	# Use this method to train the classifier. The more
+	# you train it, the better the prediction will be.
+	# 
+	# x_train is an array of features (like [[1, 2], [5, 9], [6, 8], [2, 3]])
+	# y_train is an array of labels   (like [  'a' ,  'b'  ,  'b'  ,  'a'  ])
+	#         labels index must match the corresponding feature index
+	def train(self, x_train, y_train):
+		self.x_train = x_train
+		self.y_train = y_train
+
+	# Use this method to predit output 
+	def predict(self,x_test):
+		predictions = []
+
+		for j in range(len(x_test)):
+			neighbors = self.getNeighbors(x_test[j])
+			labels = {}
+			for i in range(self.k):
+				label = self.y_train[neighbors[i]]
+				if label in labels:
+					labels[label] += 1
+				else:
+					labels[label] = 1
+			sortedLabels = sorted(labels.iteritems(), key=operator.itemgetter(1), reverse=True)
+			predictions.append(sortedLabels[0][0])
+		return predictions
+
+	def getNeighbors(self, point):
+		distances = []
+
+		for i in range(len(self.x_train)):
+			dist = euclideanDistance(point, self.x_train[i])
+			distances.append((dist, i))
+
+		distances.sort(key=operator.itemgetter(0))
+		neighbors = []
+
+		for i in range(self.k):
+			neighbors.append(distances[i][1])
+
+		return neighbors
+
+
+def getAccuracy(y_test, y_good):
+	good = 0
+	for x in range(len(y_test)):
+		if y_test[x] == y_good[x]:
+			good += 1
+	return (good / float(len(y_test)))*100.0
+
+if __name__ == '__main__':
+	classifier = MyKNN()
+	train = [[1,1,1],[5,5,5],[8,8,8],[3,3,3],[10,10,10],[11,11,11]]
+	y=['a','a','b','a','b','b']
+	test = [[9,9,9], [1,1,1]]
+
+	classifier.train(train, y)
+	print(classifier.predict(test))
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from sklearn import datasets
-from MyNN import MyNN
+import time
+start_time = time.time()
 
-# We get the Iris dataset from the sklearn library
+from sklearn import datasets
+from MyKNN import *
+
 iris = datasets.load_iris()
 
-# Features in x
-# and labels in y
 x = iris.data
 y = iris.target
 
-# We split our Iris dataset in two, one half to train our classifier, 
-# and the other half to test its accuracy
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .5)
 
-my_classifier = MyNN()
-# We train the classifier with the first half of our data
+#from sklearn.neighbors import KNeighborsClassifier
+my_classifier = MyKNN()
+my_classifier.k = 5
 my_classifier.train(x_train, y_train)
-# We test it with features of the other half
+
 predictions = my_classifier.predict(x_test)
-# We compute the accuracy by comparing the output results with reals ones
-from sklearn.metrics import accuracy_score
-print("Accuracy : " + str(accuracy_score(y_test, predictions)))
+
+duration = time.time()-start_time
+print("Precision : {}% en {} sec".format(getAccuracy(y_test, predictions), duration))
